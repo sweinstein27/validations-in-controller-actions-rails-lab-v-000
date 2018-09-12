4b9b1adbb63c8aa234443d3b48f17d2216246865
Scott Weinstein <sweinstein27@gmail.com>
2018-09-12 13:51:54 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, length: {minimum: 1 }
+  validates :category, inclusion: { in %w(Fiction Non-Fiction)}
+  validates :content, length: {minimum: 100 }
 end
